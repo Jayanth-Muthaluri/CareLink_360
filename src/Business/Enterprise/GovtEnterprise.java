f85d185e27b8ce439d109fe9916f88e01d2b0391
SyedHameedUddin7 <syed.hame@northeastern.edu>
2025-11-29 16:40:16 -0500

    Code Added for remaining enterprise base classes

--- src/Business/Enterprise/GovtEnterprise.java
+++ src/Business/Enterprise/GovtEnterprise.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.Enterprise;
-
+import Business.Organization.Organization;
+import Business.Role.Roles;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author talha
  */
-public class GovtEnterprise {
+public class GovtEnterprise extends Enterprise {
+
+    public GovernmentEnterprise(String name) {
+        super(name, EnterpriseType.Government);
+    }
+
+    @Override
+    public ArrayList<Roles> getSupportedRole() {
+        return null;
+    }
+
+    @Override
+    public List<Organization.Type> getAllOrganizationTypes() {
+        List<Organization.Type> orgTypes = new ArrayList<>();
+        orgTypes.add(Organization.Type.HealthcareOfficer);
+        orgTypes.add(Organization.Type.Secretary);
+        orgTypes.add(Organization.Type.Treasurer);
+
+        return orgTypes;
+    }
     
 }
