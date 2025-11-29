88dd3e2c820fde99bb32f82901029bd6343415f9
SyedHameedUddin7 <syed.hame@northeastern.edu>
2025-11-29 15:57:01 -0500

    Business Package and Database code added

--- src/Business/Ecosystem.java
+++ src/Business/Ecosystem.java
@@ -4,10 +4,57 @@
  */
 package Business;
 
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.Role.Roles;
+import Business.Role.Admin;
+import java.util.ArrayList;
+
 /**
  *
  * @author talha
  */
-public class Ecosystem {
+public class Ecosystem extends Organization{
+    private static Ecosystem ecosystem;
+    private ArrayList<Network> networks;
+    public static Ecosystem getInstance(){
+        if(ecosystem==null){
+            ecosystem=new Ecosystem();
+        }
+        return ecosystem;
+    }
     
+    public Network createAddNetworks(){
+        Network network=new Network();
+        networks.add(network);
+        return network;
+    }
+    @Override
+    public ArrayList<Roles> getSupportedRole() {
+        ArrayList<Roles> roleList=new ArrayList<Roles>();
+        roleList.add(new Admin());
+        return roleList;
+    }
+    private Ecosystem(){
+        super(null);
+        networks=new ArrayList<Network>();
+    }
+
+    public ArrayList<Network> getNetworks() {
+        return networks;
+    }
+
+    public void setNetworks(ArrayList<Network> networks) {
+        this.networks = networks;
+    }
+    
+    public boolean checkIfUserIsUnique(String userName){
+        if(!this.getUsrAccDir().checkIfUsernameIsUnique(userName)){
+            return false;
+        }
+        for(Network network:networks){
+            
+        }
+        return true;
+    }
 }
