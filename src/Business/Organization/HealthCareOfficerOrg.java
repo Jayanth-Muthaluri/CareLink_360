94a73f6826f868aa61f42cd3a36b47ac9c5f12a6
SyedHameedUddin7 <syed.hame@northeastern.edu>
2025-12-01 12:05:06 -0500

    Updated Organization Package Classes

--- src/Business/Organization/HealthCareOfficerOrg.java
+++ src/Business/Organization/HealthCareOfficerOrg.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.Organization;
-
+import Business.Role.PublicHealthOfficer;
+import Business.Role.LabAssistant;
+import Business.Role.Roles;
+import java.util.ArrayList;
 /**
  *
  * @author talha
  */
-public class HealthCareOfficerOrg {
+public class HealthCareOfficerOrg extends Organization {
+
+    public HealthCareOfficerOrg() {
+        super(Organization.Type.HealthcareOfficer.getValue());
+    }
+
+    @Override
+    public ArrayList<Roles> getSupportedRole() {
+        ArrayList<Roles> roles = new ArrayList();
+        roles.add(new PublicHealthOfficer());
+        return roles;
+    }
+
     
 }
--- src/Business/Organization/InsurancePolicyPlannerOrg.java
+++ src/Business/Organization/InsurancePolicyPlannerOrg.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.Organization;
-
+import Business.Role.InsurancePolicyManager;
+import Business.Role.Roles;
+import java.util.ArrayList;
 /**
  *
  * @author talha
  */
-public class InsurancePolicyPlannerOrg {
+public class InsurancePolicyPlannerOrg extends Organization {
+
+    public InsurancePolicyPlannerOrg() {
+        super(Organization.Type.InsurancePolicyPlanner.getValue());
+    }
+
+    @Override
+    public ArrayList<Roles> getSupportedRole() {
+        ArrayList<Roles> roles = new ArrayList();
+        roles.add(new InsurancePolicyManager());
+        return roles;
+    }
     
 }
