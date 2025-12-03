1e228b06fb735abadf77305698c1d5df024d9f83
GaganaAnanda <ananda.ga@northeastern.edu>
2025-12-02 23:03:03 -0500

    Update Role and PatientRole classes

--- src/Business/Role/Roles.java
+++ src/Business/Role/Roles.java
@@ -4,10 +4,60 @@
  */
 package Business.Role;
 
+import Business.Ecosystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author gaganaananda
  */
-public class Roles {
+public abstract class Roles {
+    
+    public enum RoleType {
+        Admin("Admin"),
+        Doctor("Doctor"),
+        Patient("Patient"),
+        LabAssistant("Lab Assistant"),
+        Accountant("Accountant"),
+        InsuranceRepresentative("Insurance Representative"),
+        InsurancePolicyManager("Insurance Policy Manager"),
+        InsuranceFinanceSupervisor("Insurance Finance Supervisor"),
+        PublicHealthOfficer("Public Health Officer"),
+        PublicTreasurer("Public Treasure"),
+        PublicSecretaries("Public Secretaries"),
+        BloodBankManager("Blood Bank Manager"),
+        NGOAdmin("NGO Admin"),
+        NGODirector("NGO Director");
+
+        private String value;
+
+        private RoleType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    public abstract JPanel createWorkArea(JPanel userProcessContainer,
+            UserAccount account,
+            Organization organization,
+            Enterprise enterprise,
+            Ecosystem ecosystem);
+
+    @Override
+    public String toString() {
+
+        return this.getClass().getName();
+    }
     
 }
